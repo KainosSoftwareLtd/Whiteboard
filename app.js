f62c09e0eba7b40b9b583614fc7646a5beb0b70c
Michael Kemp <mkemp01@qub.ac.uk>
2015-11-18 20:47:34 +0000

    added fabricjs library, added test page to test out canvas

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var canvas = require('./routes/canvastest');
 
 var app = express();
 
@@ -25,6 +26,7 @@ app.use(express.static(path.join(__dirname, 'node_modules')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/test', canvas);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
