b032fdf602e1fa3ee3802105d0a4cc6859a53bd9
Michael Kemp <mkemp01@qub.ac.uk>
2015-11-29 16:28:58 +0000

    fixed issue with socket.io causing easyRTC to not work

--- public/javascript/fabrictest.js
+++ public/javascript/fabrictest.js
@@ -1,24 +1,29 @@
 window.onload = function () {
-    //var $ = function(id){return document.getElementById(id)};
 
-    var socket = io();
+    var mysocket = io.connect(null, {
+        'connect timeout': 10000,
+        'force new connection': true
+    });
+
+    if (!mysocket) {
+        throw "io.connect failed";
+    }
+    else {
+        console.log("application allocated socket ", mysocket);
+        easyrtc.useThisSocketConnection(mysocket);
+    }
 
     var canvas = this.__canvas = new fabric.Canvas('c', {
         isDrawingMode: true
     });
 
-    var canvas2 = new fabric.Canvas('c2');
-
     canvas.on('mouse:up', function(options){
         //console.log(JSON.stringify(canvas));
         var x = JSON.stringify(canvas.toDatalessJSON());
-        socket.emit('canvas data', x);
-        
-
+        mysocket.emit('canvas data', x);
     });
 
-    socket.on('canvas data', function(data) {
+    mysocket.on('canvas data', function(data) {
         canvas.loadFromJSON(data, canvas.renderAll.bind(canvas));
     });
-
 };
