b5d533e5c3a419f63dd3e915c466b444f541fa42
Michael Kemp <mkemp01@qub.ac.uk>
2015-12-21 12:52:17 +0000

    added join room method
    fixed issue with zindex and removed unused files
    
    tidied up controller code

--- public/easyrtc/dev/scripts/update_client_easyrtc_ft_docs.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-jsdoc -d ../../docs/client_html_docs  -t client_jsdoc_templates2 ../../api/easyrtc_ft.js --verbose
--- public/js/controllers/BoardCtrl.js
+++ public/js/controllers/BoardCtrl.js
@@ -1,14 +1,81 @@
-angular.module('BoardCtrl', []).controller('BoardCtrl', ['$scope', '$routeParams', 'EasyRTCService', function($scope, $routeParams, EasyRTCService) {
+angular.module('BoardCtrl', []).controller('BoardCtrl', ['$scope', '$routeParams', function($scope, $routeParams) {
 
     var roomId = $routeParams.roomId;
 
+    var maxCALLERS = 4;
 
     var socket = initSocket();
     var canvas = initCanvas();
+    initRTC();
 
-    EasyRTCService.init('box0', roomId);
-    EasyRTCService.setClientVideoStream('box1')
+    function initRTC() {
+        //easyrtc.enableDebug(true);
+        console.log("Initializing.");
 
+        easyrtc.setRoomOccupantListener(roomListener);
+        var connectSuccess = function (myId) {
+            console.log("My easyrtcid is " + myId);
+        };
+
+        var connectFailure = function (errorCode, errText) {
+            console.log(errText);
+        };
+        easyrtc.initMediaSource(
+            function () {        // success callback
+                var selfVideo = document.getElementById("myVideo");
+                easyrtc.setVideoObjectSrc(selfVideo, easyrtc.getLocalStream());
+                easyrtc.joinRoom(roomId);
+                easyrtc.connect("kainos-whiteboard", connectSuccess, connectFailure);
+            },
+            connectFailure
+        );
+    }
+
+    easyrtc.setStreamAcceptor( function(callerEasyrtcid, stream) {
+        var video = document.getElementById("clientVideo");
+        easyrtc.setVideoObjectSrc(video, stream);
+    });
+
+    easyrtc.setOnStreamClosed( function (callerEasyrtcid) {
+        easyrtc.setVideoObjectSrc(document.getElementById('caller'), "");
+    });
+
+    //REF:https://easyrtc.com/docs/guides/easyrtc_client_tutorial.php
+    function roomListener(roomName, otherPeople) {
+
+        easyrtc.setRoomOccupantListener(null); // so we're only called once.
+
+        var list = [];
+        var connectCount = 0;
+        for(var easyrtcid in otherPeople ) {
+            list.push(easyrtcid);
+        }
+        //
+        // Connect in reverse order. Latter arriving people are more likely to have
+        // empty slots.
+        //
+        function establishConnection(position) {
+
+            function callSuccess() {
+
+                connectCount++;
+                if( connectCount < maxCALLERS && position > 0) {
+                    establishConnection(position-1);
+                }
+            }
+            function callFailure(errorCode, errorText) {
+                easyrtc.showError(errorCode, errorText);
+                if( connectCount < maxCALLERS && position > 0) {
+                    establishConnection(position-1);
+                }
+            }
+            easyrtc.call(list[position], callSuccess, callFailure);
+
+        }
+        if( list.length > 0) {
+            establishConnection(list.length-1);
+        }
+    }
 
     function initSocket(){
         var socket = io.connect(null, {
