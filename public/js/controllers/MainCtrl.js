df09e58307503886c8f26ea87d6e3160261f1fca
Michael Kemp <mkemp01@qub.ac.uk>
2015-12-22 14:41:34 +0000

    added basic hompage and controller

--- public/js/controllers/MainCtrl.js
+++ public/js/controllers/MainCtrl.js
@@ -1,5 +1,30 @@
-angular.module('MainCtrl', []).controller('MainController', function($scope) {
+angular.module('MainCtrl', []).controller('MainController', function($scope, $location) {
 
-    $scope.tagline = 'To the moon and back!';
+    $scope.inputField = '';
+    $scope.buttonDisabled = true;
+
+
+    $scope.go = function() {
+        $location.path('/board/' + $scope.inputField);
+    };
+
+    $scope.createNewMeeting = function() {
+
+    };
+
+    $scope.addNumberToDisplay = function(num) {
+
+        if($scope.inputField.length > 3){
+            $scope.inputField = '';
+        }
+
+        if($scope.inputField.length == 3) {
+            $scope.buttonDisabled = false;
+        } else {
+            $scope.buttonDisabled = true;
+        }
+
+        $scope.inputField += num;
+    };
 
 });
