08965734993a73e02a2085aa3f57d9727697053e
Michael Kemp <mkemp01@qub.ac.uk>
2016-01-02 17:07:52 +0000

    Added basic functionality to send ical invite as email

--- public/js/controllers/InviteCtrl.js
+++ public/js/controllers/InviteCtrl.js
@@ -1,3 +1,6 @@
-angular.module('InviteCtrl', []).controller('InviteCtrl', function($scope) {
+angular.module('InviteCtrl', []).controller('InviteCtrl', ['$http'], function($scope, $http) {
+
+    var randomRoomNumber = Math.floor(1000 + Math.random() * 9000);
+
 
 });
--- routes/invite.js
+++ routes/invite.js
@@ -1,9 +1,54 @@
 var express = require('express');
 var router = express.Router();
+var nodemailer = require('nodemailer');
+var transporter = nodemailer.createTransport('***REMOVED***');
+var ical = require('ical-generator');
 
-/* GET home page. */
+
+
+/* GET invite page. */
 router.get('/', function(req, res, next) {
     res.render('invite');
+});
+
+router.post('/', function (req, res, next) {
+
+    var cal = ical();
+
+    var event = cal.createEvent({
+        start: new Date(),
+        end: new Date(new Date().getTime() + 3600000),
+        timestamp: new Date(),
+        summary: 'My Event',
+        organizer: 'Sebastian Pekarek <mail@example.com>'
+    });
+
+    var invite = cal.toString();
+
+    var mailOptions = {
+        from: 'Fred Foo ✔ <technoir08@gmail.com>', // sender address
+        to: '***REMOVED***, ***REMOVED***', // list of receivers
+        subject: 'Hello ✔', // Subject line
+        attachments: [
+            {
+                filename: 'invite.ics',
+                content: invite,
+                contentType: 'text/calendar'
+            }
+        ],
+
+        html: '<b>Hello world ✔</b>'
+    };
+
+    transporter.sendMail(mailOptions, function(error, info){
+        if(error){
+            return console.log(error);
+        }
+        console.log('Message sent: ' + info.response);
+
+    });
+
+    next();
 });
 
 module.exports = router;
